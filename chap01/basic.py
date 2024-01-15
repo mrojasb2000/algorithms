3171218b35c1a06da6153b88016e97e5c8446681
Mauricio Rojas <mrojasb2000@gmail.com>
2024-01-14 22:40:45 -0300

    feat: sum values by functional operations

--- chap01/basic.py
+++ chap01/basic.py
@@ -1,29 +1,26 @@
-from collections.abc import Sequence
-age = int(input('Enter your age un years: '))
-max_heart_rate = 206.9 - (0.67 * age)
-target = 0.65 * max_heart_rate
-print('Your target fat-buring heart rate is', target)
+from collections.abc import Sequence, Callable
 
-
-def sum_number(limit: int = 10) -> int:
-    s = 0
-    for n in range(1, limit):
-        if n % 3 == 0 or n % 5 == 0:
-            s += n
-    return s
-
-
-print('Data: ', sum_number())
-
-data = [7, 11]
-
+def until(limit: int, 
+           filter_func: Callable[[int], bool],
+           v: int) -> list[int]:
+    if v == limit:
+        return []
+    elif filter_func(v):
+        return [v] + until(limit, filter_func, v + 1)
+    else:
+        return until(limit, filter_func, v + 1)
 
 def sumr(seq: Sequence[int]) -> int:
     if len(seq) == 0:
         return 0
     return seq[0] + sumr(seq[1:])
 
+def mul_3_5(x: int) -> bool:
+    return x % 3 == 0 or x % 5 == 0
 
-print('Data: ', sumr(data))
+def sum_functional(limit: int = 10) -> int:
+    return sumr(until(limit, mul_3_5, 0))
 
-print('Data: ', sumr([]))
+print(until(10, mul_3_5, 0))
+
+print(sum_functional())
